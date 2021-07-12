19ebe548d2b2a2ec231aafce1c162b32a0e2d147
IbragimovAdel <ibragimov.adel.kzn@gmail.com>
2021-07-12 14:14:51 +0300

    add form

--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
-import { random } from './utils';
-
-console.log('hello world');
-
-const request = (id) => {
-	return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-};
+import './style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-	const randomId = random(1, 200);
-	request(randomId)
-		.then((response) => response.json())
-		.then((json) => {
-			const pre = document.createElement('pre');
-			pre.innerText = JSON.stringify(json);
-			document.body.appendChild(pre);
-		})
-		.catch((error) => alert(error));
+	const list = document.querySelector('#list ul');
+	const nameInput = document.querySelector('input[name="name"]');
+	const trafficInput = document.querySelector('input[name="traffic"]');
+	const button = document.querySelector('#submit-form');
+
+	const data = [];
+
+	button.addEventListener('click', addElement);
+
+	renderList();
+
+	function addElement() {
+		const name = nameInput.value;
+		const traffic = trafficInput.value;
+
+		if (!name) {
+			nameInput.classList.add('error');
+			return;
+		}
+		nameInput.classList.remove('error');
+
+		if (!traffic) {
+			trafficInput.classList.add('error');
+			return;
+		}
+		trafficInput.classList.remove('error');
+
+		data.push({ name, traffic });
+		nameInput.value = '';
+		trafficInput.value = '';
+		renderList();
+	}
+
+	function renderList() {
+		list.innerHTML = '';
+		let li;
+		if (!data.length) {
+			li = document.createElement('li');
+			li.textContent = 'Список пуст';
+			list.append(li);
+		} else {
+			data.forEach((el) => {
+				li = document.createElement('li');
+				li.textContent = `${el.name} - ${el.traffic}Гб`;
+				list.append(li);
+			});
+		}
+	}
 });
